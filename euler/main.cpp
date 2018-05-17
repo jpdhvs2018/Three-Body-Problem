528849ce3a964d480f76faf7705845b132986f9a
Victor Sanchez <vsanchezb@unal.edu.co>
2018-05-17 16:49:01 -0500

    todos menos jesus

--- euler/main.cpp
+++ euler/main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
   const double ti=0.0;
-  const double te=10.0;//17.0652165601579625588917206249;
+  const double te=17.0652165601579625588917206249;
   std::vector<double> position;
   std::vector<double> velocity;
   initial_condition(position, velocity);
--- rk4/rk4-sim.cpp
+++ rk4/rk4-sim.cpp
@@ -17,7 +17,7 @@ void print(const std::vector<double> & v)
 {
   for (int i = 0; i < v.size(); ++i)
     {
-      std::cout << v[i] << "   ";
+      std::cout << v[i] << " ";
     }
 }
 
@@ -59,114 +59,63 @@ double compute(const std::vector<double> & pos, const std::vector<double> & vel,
     exit(1);
   }
 }
-//pegué la funcion RK4 para hacerle cambios y adaptarla anuestro problema, actividad que no se ha hecho...
-void euler(std::vector<double> & pos, std::vector<double> & vel, const double tini, const double tend)
-{
-  std::vector<double> posaux(pos.size());
-  std::vector<double> velaux(vel.size());
-  double dt = T/(24000.0);
-  int N = (int)((tend-tini)/dt);// preguntar
-  for (int i = 0; i < N; ++i)
-    {
-      double t = tini +  dt*i;
-      std::copy(vel.begin(), vel.end(), velaux.begin());
-      std::copy(pos.begin(), pos.end(), posaux.begin());
-      for (int j = 0; j < (vel.size()) ; ++j)
-	{
-	  vel[j] = vel[j] + dt*compute(posaux, velaux, t, j+1); 
-	}
-      
-      for (int k = 0; k < (pos.size()) ; ++k)
-	{
-	  pos[k] = pos[k] + dt*compute(posaux, velaux, t, k+4);
-	}
-      
-      std::cout << t << "   ";
-      print(pos);
-      print(vel);
-      std::cout << "\n";
-    }
-}
+
 void rk4(std::vector<double> & pos, std::vector<double> & vel, const double tini, const double tend)
 {
-  std::vector<double> k1, k2, k3, k4, aux;
-  k1.resize(vel.size());
-  k2.resize(vel.size());
-  k3.resize(vel.size());
-  k4.resize(vel.size());
-  aux.resize(vel.size());
-  const double dt=T/6000;
-  const int N = (tend-tini)/dt;
+  std::vector<double> k1, k2, k3, k4, vaux, paux;
+  k1.resize(6);
+  k2.resize(6);
+  k3.resize(6);
+  k4.resize(6);
+  vaux.resize(vel.size());
+  paux.resize(vel.size());
+  const double dt=T/6000.0;
+  const int N =(int)(tend-tini)/dt;
+
+  //calculo
   for (int nt = 0; nt < N; ++nt) {
-    //implementacion de rk4 para calcular la velocidad;
-    double t = tini + dt*nt; 
-    // k1
-    for(int ii = 0; ii < vel.size(); ++ii) {
-      k1[ii] = dt*compute(pos, vel,t, ii+1);
-    }
-    // k2 aux
-    for(int ii = 0; ii < vel.size(); ++ii) {
-      aux[ii] = vel[ii] + k1[ii]/2;
-    }
-    //k2
-    for(int ii = 0; ii < vel.size(); ++ii) {
-      k2[ii] = dt*compute(pos,aux,t+dt/2, ii+1);
-    }
-    // k3 aux
-    for(int ii = 0; ii < vel.size(); ++ii) {
-      aux[ii] = vel[ii] + k2[ii]/2;
-    }
-    //k3
-    for(int ii = 0; ii < vel.size(); ++ii) {
-      k3[ii] = dt*compute(pos,aux,t+dt/2, ii+1);
-    }
-    // k4 aux
-    for(int ii = 0; ii < vel.size(); ++ii) {
-      aux[ii] = vel[ii] + k3[ii];
-    }
-    //k4
-    for(int ii = 0; ii < vel.size(); ++ii) {
-      k4[ii] = dt*compute(pos, aux, t + dt, ii+1);
-    }
-    // new vel
-    for(int ii = 0; ii < vel.size(); ++ii) {
-      vel[ii] = vel[ii] + (k1[ii] + 2*k2[ii] + 2*k3[ii] + k4[ii])/6.0;
-    }
+    double t = tini + dt*nt;
     
-    //* implementacion de rk4 para calcular la posicion.
     // k1
-    for(int ii = 0; ii < pos.size(); ++ii) {
+    for(int ii = 0; ii < vel.size(); ++ii) {
+      k1[ii+3] = dt*compute(pos, vel,t, ii+1);
       k1[ii] = dt*compute(pos, vel,t, ii+4);
     }
     // k2 aux
-    for(int ii = 0; ii < pos.size(); ++ii) {
-      aux[ii] = pos[ii] + k1[ii]/2;
+    for(int ii = 0; ii < vel.size(); ++ii) {
+      vaux[ii] = vel[ii] + k1[ii+3]/2;
+      paux[ii] = pos[ii] + k1[ii]/2;
     }
     //k2
-    for(int ii = 0; ii < pos.size(); ++ii) {
-      k2[ii] = dt*compute(aux,vel,t+dt/2, ii+4);
+    for(int ii = 0; ii < vel.size(); ++ii) {
+     k2[ii+3] = dt*compute(paux,vaux,t+dt/2, ii+1);
+     k2[ii] = dt*compute(paux,vaux,t+dt/2, ii+4);
     }
     // k3 aux
-    for(int ii = 0; ii < pos.size(); ++ii) {
-      aux[ii] = pos[ii] + k2[ii]/2;
+    for(int ii = 0; ii < vel.size(); ++ii) {
+      vaux[ii] = vel[ii] + k2[ii+3]/2;
+      paux[ii] = pos[ii] + k2[ii]/2;
     }
     //k3
-    for(int ii = 0; ii < pos.size(); ++ii) {
-      k3[ii] = dt*compute(aux,vel,t+dt/2, ii+4);
+    for(int ii = 0; ii < vel.size(); ++ii) {
+      k3[ii+3] = dt*compute(paux,vaux,t+dt/2, ii+1);
+      k3[ii] = dt*compute(paux,vaux,t+dt/2, ii+4);
     }
     // k4 aux
-    for(int ii = 0; ii < pos.size(); ++ii) {
-      aux[ii] = pos[ii] + k3[ii];
+    for(int ii = 0; ii < vel.size(); ++ii) {
+      vaux[ii] = vel[ii] + k3[ii+3];
+      paux[ii] = pos[ii] + k3[ii];
     }
     //k4
-    for(int ii = 0; ii < pos.size(); ++ii) {
-      k4[ii] = dt*compute(aux, vel, t + dt, ii+4);
+    for(int ii = 0; ii < vel.size(); ++ii) {
+      k4[ii+3] = dt*compute(paux, vaux, t + dt, ii+1);
+      k4[ii] = dt*compute(paux, vaux, t + dt, ii+4);
     }
     // new vel
-    for(int ii = 0; ii < pos.size(); ++ii) {
+    for(int ii = 0; ii < vel.size(); ++ii) {
+      vel[ii] = vel[ii] + (k1[ii+3] + 2*k2[ii+3] + 2*k3[ii+3] + k4[ii+3])/6.0;
       pos[ii] = pos[ii] + (k1[ii] + 2*k2[ii] + 2*k3[ii] + k4[ii])/6.0;
     }
-    
     
     std::cout << t << " ";
     print(pos);
